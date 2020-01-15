e1b594fe000308c39deee1b14404bc4733d3b787
root <rafaelssilveira@outlook.com>
2020-01-15 14:47:50 +0000

    add passport userschema

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,5 +1,10 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const User = require('./models/user')
+const passport = require('passport')
+const bodyParser = require('body-parser')
+const LocalStrategy = require ('passport-local')
+const passportLocalMongoose = require('passport-local-mongoose')
 
 
 mongoose.connect("mongodb+srv://omnistack:omnistack@cluster0-se1sy.mongodb.net/AuthDemo?retryWrites=true&w=majority", {
@@ -9,9 +14,20 @@ mongoose.connect("mongodb+srv://omnistack:omnistack@cluster0-se1sy.mongodb.net/A
   // mongoose.Promise = global.Promise;
 });
 const app = express()
-
 app.set("view engine", "ejs")
 
+
+app.use(require('express-session')({
+	secret:"Bula the best dog",
+	resave:false,
+	saveUninitialized:false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser)
 app.get('/',(req,res)=>{
 	res.render("home")
 })
