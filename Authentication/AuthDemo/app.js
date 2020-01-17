8223f089db5e6d6d79cde75b1ebe2a07b37c9ecd
root <rafaelssilveira@outlook.com>
2020-01-17 20:37:49 +0000

    route post register

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -16,7 +16,7 @@ mongoose.connect("mongodb+srv://omnistack:omnistack@cluster0-se1sy.mongodb.net/A
 const app = express()
 app.set("view engine", "ejs")
 
-
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(require('express-session')({
 	secret:"Bula the best dog",
 	resave:false,
@@ -28,6 +28,9 @@ app.use(passport.session())
 
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser)
+
+//ROUTES:
+
 app.get('/',(req,res)=>{
 	res.render("home")
 })
@@ -35,4 +38,27 @@ app.get('/secret',(req,res)=>{
 	res.render('secret')
 })
 
-app.listen(3000)
+//AUTH ROUTES
+
+app.get('/register',(req,res)=>{
+	res.render('register')
+})
+
+app.post('/register',(req,res)=>{
+	req.body.username
+	req.body.password
+	//
+	User.register(new User({username: req.body.username}), req.body.password, (err,user)=>{
+		if(err){
+		   console.log(err)
+			return res.render('register')
+		   }
+		passport.authenticate('local')(req,res ,function(){
+			res.redirect('/secret')
+		})
+	})
+	
+})
+
+
+app.listen(3333)
