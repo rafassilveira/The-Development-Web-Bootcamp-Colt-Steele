3cc197ab77f355474f5e01f514b8bd4827111789
Rafael Silveira <rafaelssilveira@outlook.com>
2020-01-20 14:20:15 -0300

    add form route

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,64 +1,87 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const User = require('./models/user')
-const passport = require('passport')
-const bodyParser = require('body-parser')
-const LocalStrategy = require ('passport-local')
-const passportLocalMongoose = require('passport-local-mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
+const User = require("./models/user");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const LocalStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
 
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@cluster0-se1sy.mongodb.net/AuthDemo?retryWrites=true&w=majority",
+  {
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useNewUrlParser: true
+    // mongoose.Promise = global.Promise;
+  }
+);
+const app = express();
+app.set("view engine", "ejs");
 
-mongoose.connect("mongodb+srv://omnistack:omnistack@cluster0-se1sy.mongodb.net/AuthDemo?retryWrites=true&w=majority", {
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useNewUrlParser: true
-  // mongoose.Promise = global.Promise;
-});
-const app = express()
-app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  require("express-session")({
+    secret: "Bula the best dog",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(require('express-session')({
-	secret:"Bula the best dog",
-	resave:false,
-	saveUninitialized:false
-}))
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(passport.initialize())
-app.use(passport.session())
-
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser)
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser);
 
 //ROUTES:
 
-app.get('/',(req,res)=>{
-	res.render("home")
-})
-app.get('/secret',(req,res)=>{
-	res.render('secret')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/secret", (req, res) => {
+  res.render("secret");
+});
 
 //AUTH ROUTES
 
-app.get('/register',(req,res)=>{
-	res.render('register')
-})
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
-app.post('/register',(req,res)=>{
-	req.body.username
-	req.body.password
-	//
-	User.register(new User({username: req.body.username}), req.body.password, (err,user)=>{
-		if(err){
-		   console.log(err)
-			return res.render('register')
-		   }
-		passport.authenticate('local')(req,res ,function(){
-			res.redirect('/secret')
-		})
-	})
-	
-})
+//handling user sign up
+app.post("/register", (req, res) => {
+  req.body.username;
+  req.body.password;
+  //We having passing password as second paramenter, because we'll hashing
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        return res.render("register");
+      }
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secret");
+      });
+    }
+  );
+});
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-app.listen(3333)
+//login logic
+app.post(
+  "/login",
+  //middleware
+  passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+  }),
+  (req, res) => {}
+);
+
+app.listen(3333);
